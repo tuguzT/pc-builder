320b0c453ff8f93f9dd5bcb59c596e38b3ece666
tuguzT <timurka.tugushev@gmail.com>
2021-10-19 12:57:56 +0300

    Configure Kotlin for 'domain' module

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     kotlin("jvm")
 }
@@ -7,6 +9,20 @@ java {
     targetCompatibility = JavaVersion.VERSION_1_8
 }
 
+repositories {
+    mavenCentral()
+}
+
 dependencies {
+    // Kotlin
+    implementation(kotlin("stdlib-jdk8"))
+
+    // Third-Party
     implementation("io.nacular.measured:measured:0.3.0")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
 }
