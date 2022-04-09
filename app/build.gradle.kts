e314db24046c2c263bdb77f1dcb553f002c92335
tuguzT <timurka.tugushev@gmail.com>
2022-04-09 23:38:51 +0300

    Set target SDK to 32

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 }
 
 android {
-    compileSdk = 31
+    compileSdk = 32
 
     defaultConfig {
         applicationId = "io.github.tuguzt.pcbuilder"
         minSdk = 21
-        targetSdk = 31
+        targetSdk = 32
         versionCode = 1
         versionName = "1.0"
 
