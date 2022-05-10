75b46eac9a2dad9a184b2352fb1f9a0917c55567
tuguzT <timurka.tugushev@gmail.com>
2022-05-10 17:16:04 +0300

    Improve component details screen

--- app/src/main/java/io/github/tuguzt/pcbuilder/view/components/ComponentDetailsScreen.kt
+++ app/src/main/java/io/github/tuguzt/pcbuilder/view/components/ComponentDetailsScreen.kt
@@ -1,41 +1,118 @@
 package io.github.tuguzt.pcbuilder.view.components
 
 import android.content.res.Configuration
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.ColorPainter
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import io.github.tuguzt.pcbuilder.domain.model.component.Component
 import io.github.tuguzt.pcbuilder.domain.model.component.ComponentData
 import io.github.tuguzt.pcbuilder.domain.model.component.Size
 import io.github.tuguzt.pcbuilder.view.theme.PCBuilderTheme
-import io.nacular.measured.units.Length
-import io.nacular.measured.units.Mass
 import io.nacular.measured.units.times
+import io.github.tuguzt.pcbuilder.R
+import io.nacular.measured.units.Length.Companion.millimeters
+import io.nacular.measured.units.Mass.Companion.grams
+import io.nacular.measured.units.Mass.Companion.kilograms
 
 /**
  * Application screen with information about provided [component].
  */
 @Composable
 fun ComponentDetailsScreen(component: Component) {
-    Column(modifier = Modifier.fillMaxSize()) {
-        Text(text = "Name: ${component.name}")
-        Spacer(modifier = Modifier.height(8.dp))
-        Text(text = "Description: ${component.description}")
-        Spacer(modifier = Modifier.height(8.dp))
-        Text(text = "Weight: ${component.weight}")
-        Spacer(modifier = Modifier.height(8.dp))
-        Text(text = "Length: ${component.size.length}")
-        Spacer(modifier = Modifier.height(8.dp))
-        Text(text = "Width: ${component.size.width}")
-        Spacer(modifier = Modifier.height(8.dp))
-        Text(text = "Height: ${component.size.height}")
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState()),
+    ) {
+        Image(
+            modifier = Modifier.height(240.dp),
+            painter = ColorPainter(MaterialTheme.colors.onSurface.copy(alpha = 0.2f)),
+            contentDescription = stringResource(R.string.component_picture),
+        )
+
+        Column(
+            modifier = Modifier
+                .padding(8.dp)
+                .fillMaxSize(),
+        ) {
+            Text(
+                text = component.name,
+                style = MaterialTheme.typography.h5,
+                fontWeight = FontWeight.Bold,
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = component.description,
+                style = MaterialTheme.typography.body2,
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Divider()
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = stringResource(R.string.weight),
+                style = MaterialTheme.typography.caption,
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = "${component.weight `in` grams} ${stringResource(R.string.unit_gram)}",
+                style = MaterialTheme.typography.body1,
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Divider()
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = stringResource(R.string.length),
+                style = MaterialTheme.typography.caption,
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = "${component.size.length `in` millimeters} " +
+                        stringResource(R.string.unit_millimeter),
+                style = MaterialTheme.typography.body1,
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Divider()
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = stringResource(R.string.width),
+                style = MaterialTheme.typography.caption,
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = "${component.size.width `in` millimeters} " +
+                        stringResource(R.string.unit_millimeter),
+                style = MaterialTheme.typography.body1,
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Divider()
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = stringResource(R.string.height),
+                style = MaterialTheme.typography.caption,
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = "${component.size.height `in` millimeters} " +
+                        stringResource(R.string.unit_millimeter),
+                style = MaterialTheme.typography.body1,
+            )
+        }
     }
 }
 
@@ -55,11 +132,11 @@ private fun ComponentDetailsScreenPreview() {
                     " The card has 2,560 CUDA cores with a base clock of 1.55 GHz and" +
                     " a boost clock of 1.78 GHz. It also has a 128-bit memory interface width" +
                     " seen in GPUs targeting 1080p.",
-            weight = 1 * Mass.kilograms,
+            weight = 1 * kilograms,
             size = Size(
-                length = 242 * Length.millimeters,
-                width = 112 * Length.millimeters,
-                height = 40 * Length.millimeters,
+                length = 242 * millimeters,
+                width = 112 * millimeters,
+                height = 40 * millimeters,
             ),
         )
         Surface {
